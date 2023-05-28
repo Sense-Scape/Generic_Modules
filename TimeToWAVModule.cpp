cabf51822287ed8985f00c95176c6f97484d66c6
Grabt234 <gnorrie12@gmail.com>
2023-05-28 13:59:31 +0200

    Intended to closehttps://github.com/Sense-Scape/Windows_Proc_Serv/issues/4

--- TimeToWAVModule.cpp
+++ TimeToWAVModule.cpp
@@ -21,22 +21,27 @@ void TimeToWAVModule::Process(std::shared_ptr<BaseChunk> pBaseChunk)
 
 void TimeToWAVModule::ConvertTimeToWAV(std::shared_ptr<TimeChunk> pTimeChunk, std::shared_ptr<WAVChunk> pWAVChunk)
 {
+    // Setting timestamp
+    pWAVChunk->m_i64TimeStamp = pTimeChunk->m_i64TimeStamp;
+
     // Creating WAV header
     pWAVChunk->m_sWAVHeader.SamplesPerSec = pTimeChunk->m_dSampleRate;
     pWAVChunk->m_sWAVHeader.NumOfChan = pTimeChunk->m_uNumChannels;
     pWAVChunk->m_sWAVHeader.bitsPerSample = pTimeChunk->m_uNumBytes * 8;
     pWAVChunk->m_sWAVHeader.SamplesPerSec = pTimeChunk->m_dSampleRate;
-    pWAVChunk->m_sWAVHeader.blockAlign = 2; // ask gpt
+    pWAVChunk->m_sWAVHeader.blockAlign = pTimeChunk->m_uNumBytes*pTimeChunk->m_uNumChannels;
     pWAVChunk->m_sWAVHeader.bytesPerSec = pTimeChunk->m_dSampleRate * pWAVChunk->m_sWAVHeader.blockAlign;
+
     // Setting chunk Sizes
     pWAVChunk->m_sWAVHeader.Subchunk2Size = pTimeChunk->m_uNumChannels * pTimeChunk->m_dChunkSize * pTimeChunk->m_uNumBytes;
     pWAVChunk->m_sWAVHeader.ChunkSize = pWAVChunk->m_sWAVHeader.Subchunk2Size + 44 - 8; 
 
+    // Now lets store the time data
     for (unsigned uSampleIndex = 0; uSampleIndex < pTimeChunk->m_dChunkSize; uSampleIndex++)
     {
         // Iterating through each audio channel
-        for (auto vADCChannelData = pTimeChunk->m_vvfTimeChunks.begin(); vADCChannelData != pTimeChunk->m_vvfTimeChunks.end(); ++vADCChannelData)
-            pWAVChunk->m_vfData.push_back((*vADCChannelData)[uSampleIndex]);
+        for (auto vADCChannelData = pTimeChunk->m_vvi16TimeChunks.begin(); vADCChannelData != pTimeChunk->m_vvi16TimeChunks.end(); ++vADCChannelData)
+            pWAVChunk->m_vi16Data.push_back((*vADCChannelData)[uSampleIndex]);
 
     }
 
