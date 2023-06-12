492a13ef55ab02cbac0a7f76da4b3a3920e3ca68
Grabt234 <gnorrie12@gmail.com>
2023-06-12 20:38:22 +0200

    Another Refactoring session proc for #5

--- SessionProcModule.cpp
+++ SessionProcModule.cpp
@@ -4,96 +4,93 @@ SessionProcModule::SessionProcModule(unsigned uBufferSize) : BaseModule(uBufferS
                                                             m_mFunctionCallbacksMap(),
                                                             m_mSessionBytes()
 {
-    RegisterSessionStates();
-    RegisterFunctionHandlers();
 }
 
 void SessionProcModule::Process(std::shared_ptr<BaseChunk> pBaseChunk)
 {
-    // Find out what chunk type one is processing
+    // Lets first check what chunk has been transmitted in this UDP chunk
     uint32_t u32ChunkType;
     auto pUDPChunk = std::static_pointer_cast<UDPChunk>(pBaseChunk);
     memcpy(&u32ChunkType, &pUDPChunk->m_vcDataChunk[9], sizeof(u32ChunkType));
 
-    // Call a function as a function of chunk type
-    if(m_mFunctionCallbacksMap.count(u32ChunkType))
-        m_mFunctionCallbacksMap[u32ChunkType](pUDPChunk);
-    else
-        std::cout << ChunkTypesUtility::toString(ChunkTypesUtility::FromU32(u32ChunkType)) + " not registered \n";
-}
+    // Then we can map keys
+    ChunkType SessionChunkType = ChunkTypesUtility::FromU32(u32ChunkType);
+    auto vu8SourceIdentifier = pUDPChunk->GetSourceIdentifier();
+    auto  pChunkHeaderState = GetPreviousSessionState(pUDPChunk, SessionChunkType);
 
-void SessionProcModule::RegisterFunctionHandlers()
-{
-    // Function to call as a function of chunk type
-    m_mFunctionCallbacksMap[ChunkTypesUtility::ToU32(ChunkType::TimeChunk)] = [this](std::shared_ptr<BaseChunk> pBaseChunk) { ProcessTimeChunkSession(pBaseChunk); };
-}
+    pChunkHeaderState->ConvertBytesToStates(pUDPChunk);
 
-void SessionProcModule::RegisterSessionStates()
-{
-    // Map that stores session state as a function of session type
-    m_mSessionModesStatesMap[SessionModeTypes::TimeChunkSession] = std::make_shared<TimeChunkSessionMode>();
-}
-
-void SessionProcModule::ProcessTimeChunkSession(std::shared_ptr<BaseChunk> pBaseChunk)
-{
-    //// TODO: Add ability to run sessions for each MAC Address
-
-    // Extract state bytes and store in session state
-    std::shared_ptr<TimeChunkSessionMode> pTimeChunkHeaderState = std::static_pointer_cast<TimeChunkSessionMode>(m_mSessionModesStatesMap[SessionModeTypes::TimeChunkSession]);
-    auto pUDPChunk = std::static_pointer_cast<UDPChunk>(pBaseChunk);
-    pTimeChunkHeaderState->ConvertBytesToStates(pUDPChunk);
-
-    // Checking all state variables
+    // Now we can check all state variables
     // Are we the first message in a sequence ?
-    bool bStartSequence = (pTimeChunkHeaderState->m_puSequenceNumber.second == 0);
+    bool bStartSequence = (pChunkHeaderState->m_puSequenceNumber.second == 0);
     // If we are we then want to know if that sequence in continuous
-    bool bSequenceContinuous = (pTimeChunkHeaderState->m_puSequenceNumber.second == pTimeChunkHeaderState->m_uPreviousSequenceNumber + 1); //intra
+    bool bSequenceContinuous = (pChunkHeaderState->m_puSequenceNumber.second == pChunkHeaderState->m_uPreviousSequenceNumber + 1); //intra
     // Now if we know about continuity, is this the last message in the sequence?
-    bool bLastInSequence = pTimeChunkHeaderState->m_pcTransmissionState.second == 1 ? true : false;
+    bool bLastInSequence = pChunkHeaderState->m_pcTransmissionState.second == 1 ? true : false;
     // Checking if this message belongs to the same sequences
-    bool SameSesession = ((pTimeChunkHeaderState->m_puSessionNumber.second == pTimeChunkHeaderState->m_uPreviousSessionNumber) || pTimeChunkHeaderState->m_puSessionNumber.second == 0); //inter
+    bool SameSesession = ((pChunkHeaderState->m_puSessionNumber.second == pChunkHeaderState->m_uPreviousSessionNumber) || pChunkHeaderState->m_puSessionNumber.second == 0); //inter
 
     if (bStartSequence)
     {
-        pTimeChunkHeaderState->m_uPreviousSessionNumber = pTimeChunkHeaderState->m_puSessionNumber.second;
+        pChunkHeaderState->m_uPreviousSessionNumber = pChunkHeaderState->m_puSessionNumber.second;
     }
     // Updating previous sequence after required for continuity/start checks
-    pTimeChunkHeaderState->m_uPreviousSequenceNumber = pTimeChunkHeaderState->m_puSequenceNumber.second;
+    pChunkHeaderState->m_uPreviousSequenceNumber = pChunkHeaderState->m_puSequenceNumber.second;
 
     // we have just started or are continuing a seuqnce so store intermediate bytes
     if (bStartSequence || (SameSesession && !bLastInSequence && bSequenceContinuous))
     {
         // If this is the start create a vector to store data
         if (bStartSequence)
-            m_mSessionBytes[SessionModeTypes::TimeChunkSession] = std::make_shared<std::vector<char>>();
+            m_mSessionBytes[vu8SourceIdentifier][SessionChunkType] = std::make_shared<std::vector<char>>();
 
         // lets get the start and end of the data and store the bytes
-        auto DataStart = pUDPChunk->m_vcDataChunk.begin() + pTimeChunkHeaderState->m_uDataStartPosition;
-        auto DataEnd = pUDPChunk->m_vcDataChunk.begin() + pTimeChunkHeaderState->m_uDataStartPosition + pTimeChunkHeaderState->m_puTransmissionSize.second;
-        std::copy(DataStart, DataEnd, std::back_inserter(*m_mSessionBytes[SessionModeTypes::TimeChunkSession]));
+        auto DataStart = pUDPChunk->m_vcDataChunk.begin() + pChunkHeaderState->m_uDataStartPosition;
+        auto DataEnd = pUDPChunk->m_vcDataChunk.begin() + pChunkHeaderState->m_uDataStartPosition + pChunkHeaderState->m_puTransmissionSize.second;
+        std::copy(DataStart, DataEnd, std::back_inserter(*m_mSessionBytes[vu8SourceIdentifier][SessionChunkType]));
     }
     else if (bLastInSequence && SameSesession && bSequenceContinuous)
     {
         // lets get the start and end of the data and store the bytes
-        auto DataStart = pUDPChunk->m_vcDataChunk.begin() + pTimeChunkHeaderState->m_uDataStartPosition;
-        auto DataEnd = pUDPChunk->m_vcDataChunk.begin() + pTimeChunkHeaderState->m_uDataStartPosition + pTimeChunkHeaderState->m_puTransmissionSize.second;
-        std::copy(DataStart, DataEnd, std::back_inserter(*m_mSessionBytes[SessionModeTypes::TimeChunkSession]));
+        auto DataStart = pUDPChunk->m_vcDataChunk.begin() + pChunkHeaderState->m_uDataStartPosition;
+        auto DataEnd = pUDPChunk->m_vcDataChunk.begin() + pChunkHeaderState->m_uDataStartPosition + pChunkHeaderState->m_puTransmissionSize.second;
+        std::copy(DataStart, DataEnd, std::back_inserter(*m_mSessionBytes[vu8SourceIdentifier][SessionChunkType]));
 
         // Creating a TimeChunk into which data shall go
-        auto pTimeChunk = std::make_shared<TimeChunk>(0,0,0,0,0,0);
-        pTimeChunk->Deserialise(m_mSessionBytes[SessionModeTypes::TimeChunkSession]);
+        auto pTimeChunk = std::make_shared<TimeChunk>(0, 0, 0, 0, 0, 0);
+        pTimeChunk->Deserialise(m_mSessionBytes[vu8SourceIdentifier][SessionChunkType]);
 
-        
+
         // Pass pointer to data on and clear stored data and state information for current session
         TryPassChunk(pTimeChunk);
-        m_mSessionModesStatesMap[SessionModeTypes::TimeChunkSession] = std::make_shared<TimeChunkSessionMode>();
-        m_mSessionBytes[SessionModeTypes::TimeChunkSession] = std::make_shared<std::vector<char>>();
+        m_mSessionModesStatesMap[vu8SourceIdentifier][SessionChunkType] = std::make_shared<TimeChunkSessionMode>();
+        m_mSessionBytes[vu8SourceIdentifier][SessionChunkType] = std::make_shared<std::vector<char>>();
     }
     else
     {
-        std::cout << std::string(__FUNCTION__) + " - WAV session chunk missed, resetting [" + std::to_string(pTimeChunkHeaderState->m_puSessionNumber.second) + ":" + std::to_string(pTimeChunkHeaderState->m_puSequenceNumber.second) + "->" + std::to_string(pTimeChunkHeaderState->m_uPreviousSequenceNumber) + "] \n";
-        m_mSessionModesStatesMap[SessionModeTypes::TimeChunkSession] = std::make_shared<TimeChunkSessionMode>();
-        m_mSessionBytes[SessionModeTypes::TimeChunkSession] = std::make_shared<std::vector<char>>();
+        std::cout << std::string(__FUNCTION__) + " - WAV session chunk missed, resetting [" + std::to_string(pChunkHeaderState->m_puSessionNumber.second) + ":" + std::to_string(pChunkHeaderState->m_puSequenceNumber.second) + "->" + std::to_string(pChunkHeaderState->m_uPreviousSequenceNumber) + "] \n";
+        m_mSessionModesStatesMap[vu8SourceIdentifier][SessionChunkType] = std::make_shared<TimeChunkSessionMode>();
+        m_mSessionBytes[vu8SourceIdentifier][SessionChunkType] = std::make_shared<std::vector<char>>();
     }
 }
 
+std::shared_ptr<TimeChunkSessionMode> SessionProcModule::GetPreviousSessionState(std::shared_ptr<BaseChunk> pBaseChunk, ChunkType chunkType)
+{
+    bool bProcessedBefore = false;
+    auto pUDPChunk = std::static_pointer_cast<UDPChunk>(pBaseChunk);
+
+
+    // If this unique identifier has been seen 
+    if (m_mSessionModesStatesMap.count(pBaseChunk->GetSourceIdentifier()))
+    {
+        // And the chunk type has been processed before
+        if (m_mSessionModesStatesMap[pBaseChunk->GetSourceIdentifier()].count(chunkType))
+            // We can then return its session state
+            return std::static_pointer_cast<TimeChunkSessionMode>(m_mSessionModesStatesMap[pBaseChunk->GetSourceIdentifier()][chunkType]);
+    }
+
+    // If we have never seen this source identifier and chunk type 
+    // then lets establish a seesion mode for it
+    m_mSessionModesStatesMap[pBaseChunk->GetSourceIdentifier()][chunkType] = std::make_shared<TimeChunkSessionMode>();
+    return std::static_pointer_cast<TimeChunkSessionMode>(m_mSessionModesStatesMap[pBaseChunk->GetSourceIdentifier()][chunkType]);
+}
