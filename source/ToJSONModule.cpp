cbe1da53bb9787f5eb86fbbef215165611d730a5
Grabt234 <gnorrie12@gmail.com>
2023-08-14 21:05:55 +0200

    Feature: Generically converting to json

--- source/ToJSONModule.cpp
+++ source/ToJSONModule.cpp
@@ -8,20 +8,12 @@ ToJSONModule::ToJSONModule()
 void ToJSONModule::Process(std::shared_ptr<BaseChunk> pBaseChunk)
 {
 	// Assuming we are just processing time chunks at the moment
-	auto pTimeChunk = std::static_pointer_cast<TimeChunk>(pBaseChunk);
-	auto pJSONChunk = std::make_shared<JSONChunk>();
+	if (ChunkToJSONConverter* pChunkToJSONConverter = dynamic_cast<ChunkToJSONConverter*>(pBaseChunk.get()))
+	{
+		auto pJSONChunk = std::make_shared<JSONChunk>();
+		pJSONChunk->m_JSONDocument = *pChunkToJSONConverter->ToJSON();
 
-	auto JSONDocument = nlohmann::json();
-	auto strChunkName = ChunkTypesNamingUtility::toString(pTimeChunk->GetChunkType());
-	JSONDocument[strChunkName]["SampleRate"] = std::to_string(pTimeChunk->m_dSampleRate);
-	JSONDocument[strChunkName]["ChunkSize"] = std::to_string(pTimeChunk->m_dChunkSize);
-	JSONDocument[strChunkName]["NumChannels"] = std::to_string(pTimeChunk->m_uNumChannels);
-
-	for (unsigned uChannelIndex = 0; uChannelIndex < pTimeChunk->m_uNumChannels; uChannelIndex++)
-		JSONDocument[strChunkName]["Channels"][std::to_string(uChannelIndex)] = pTimeChunk->m_vvi16TimeChunks[uChannelIndex];
-
-	pJSONChunk->m_JSONDocument = JSONDocument;
-
-	// Try pass on
-	TryPassChunk(pJSONChunk);
+		// Try pass on
+		TryPassChunk(pJSONChunk);
+	}
 }
