4392bdac8a38cf59ccf8b142e7e591dac3ec1b9d
Grabt234 <gnorrie12@gmail.com>
2023-08-22 07:54:46 +0200

    Feat: Adding some non funcational changes to test

--- include_tests/SessionProcModuleTests.h
+++ include_tests/SessionProcModuleTests.h
@@ -6,10 +6,21 @@
 
 TEST_CASE("SessionProcModule Test")
 {
+	// Lets start by creating the session proc module
 	unsigned uBufferSize = 10;
-	SessionProcModule sessionProcModule(uBufferSize);
+	SessionProcModule SessionProcModuleTestClass(uBufferSize);
+	SessionProcModuleTestClass.SetTestMode(true);
 
-	CHECK(sessionProcModule.GetModuleType() == ModuleType::SessionProcModule);
+	CHECK(SessionProcModuleTestClass.GetModuleType() == ModuleType::SessionProcModule);
+
+	// Create a JSON Chunk
+	auto pJSONChunk = std::make_shared<JSONChunk>();
+	pJSONChunk->m_JSONDocument["test"] = "confirmed";
+	auto pcJSONBytes = pJSONChunk->Serialise();
+
+	// Emulate a transmission using the session mode class
+
+	// Check if received class pointer is the same
 }
 
 #endif
