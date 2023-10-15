20da3974ecb94c5db0b4097322a374eb9377efe8
Grabt234 <gnorrie12@gmail.com>
2023-10-15 11:52:48 +0200

    Feat: Updating FFT tests

--- source/FFTModule.cpp
+++ source/FFTModule.cpp
@@ -8,26 +8,31 @@ FFTModule::FFTModule(unsigned uBufferSize) : BaseModule(uBufferSize)
 void FFTModule::Process(std::shared_ptr<BaseChunk> pBaseChunk)
 {
     auto pTimeChunk = std::static_pointer_cast<TimeChunk>(pBaseChunk);
-    
-    int iFFTPoints = pTimeChunk->m_dChunkSize;
-    auto pFFTChunk = std::make_shared<FFTChunk>();
+    auto pFFTChunk = std::make_shared<FFTChunk>(pTimeChunk->m_dChunkSize, pTimeChunk->m_dSampleRate, pTimeChunk->m_i64TimeStamp, pTimeChunk->m_uNumChannels);
 
     // Forward FFT configuration
-    kiss_fft_cfg ForwardFFTConfig = kiss_fft_alloc(iFFTPoints, 0, NULL, NULL);
+    kiss_fft_cfg ForwardFFTConfig = kiss_fft_alloc(pTimeChunk->m_dChunkSize, 1, NULL, NULL);
 
     // Convert to complex vector
-    std::vector<std::complex<float>> vcForwardFFTOutput(iFFTPoints, 0.0);
     pFFTChunk->m_vvcfFFTChunks.resize(pTimeChunk->m_uNumChannels);
-
+    
     // Iterate through data channel
     for (uint16_t uChannelIndex = 0; uChannelIndex  < pTimeChunk->m_uNumChannels; uChannelIndex++)
     {
-        // Store time data in FFTChunk 
+
+        std::vector<std::complex<float>> vcfForwardFFTInput;
+        std::vector<std::complex<float>> vcfForwardFFTOutput(pTimeChunk->m_dChunkSize, 0.0);
+
+        // And stoire time data in FFTChunk 
         for (auto i16SubChunkSample : pTimeChunk->m_vvi16TimeChunks[uChannelIndex])
             // Creating a complex value with only real component
-            pFFTChunk->m_vvcfFFTChunks[uChannelIndex].push_back(std::complex<float>(i16SubChunkSample, 0.0));
+            vcfForwardFFTInput.push_back(std::complex<float>(i16SubChunkSample, 0.0));
+
         // And then take FFT
-        kiss_fft(ForwardFFTConfig, (kiss_fft_cpx*)&pFFTChunk->m_vvcfFFTChunks[uChannelIndex][0], (kiss_fft_cpx*)&pFFTChunk->m_vvcfFFTChunks[uChannelIndex][0]);
+        kiss_fft(ForwardFFTConfig, (kiss_fft_cpx*)&vcfForwardFFTInput[0], (kiss_fft_cpx*)&vcfForwardFFTOutput[0]);
+
+        // And store data
+        pFFTChunk->m_vvcfFFTChunks[uChannelIndex] = vcfForwardFFTOutput;
     }
     
     // Now free up memory
